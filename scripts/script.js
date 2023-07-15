372d415a3d651bee849315ef0d37edbf612b838f
TahiryAllaoui <tallaoui31@gmail.com>
2023-07-15 20:25:25 +0300

    first update

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+const listeMots = ["Maman", "Famille", "interlocution"];
+const listePhrase = ["Annie, are you OK?", "Smooth criminal is hitting", "Mickel Jackson is a Pop Star"];
+let choixDeJeu = prompt("Voulez-vous jouer avec des mots out des phrase, (0) pour Mots et (1) pour Phrase: ");
+let score = 0;
+
+
+//functions
+const modeDeJeu = (choix) => {
+    return (choix === 0) ? listeMots : listePhrase;
+}
+
+const jeu = (liste) => {
+    for (let i = 0; i < liste.length; i++) {
+        let motUtilisateur = prompt(`Entrer: ${liste[i]}`);
+        if (liste[i] === motUtilisateur) {
+            score++;
+        }
+        else {
+            i--;
+        }
+    }
+    return score;
+}
+
+//test
+let partie = jeu(modeDeJeu(parseInt(choixDeJeu)));
+
+console.log(" votre score est: ", score);
+
