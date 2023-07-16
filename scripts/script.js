c417d69a5e10d03e36e029ae9031bf96db5ccde3
TahiryAllaoui <tallaoui31@gmail.com>
2023-07-16 08:34:41 +0300

    front-end ready

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,28 @@
-const listeMots = ["Maman", "Famille", "interlocution"];
-const listePhrase = ["Annie, are you OK?", "Smooth criminal is hitting", "Mickel Jackson is a Pop Star"];
-let choixDeJeu = prompt("Voulez-vous jouer avec des mots out des phrase, (0) pour Mots et (1) pour Phrase: ");
-let score = 0;
+// const listeMots = ["Maman", "Famille", "interlocution"];
+// const listePhrase = ["Annie, are you OK?", "Smooth criminal is hitting", "Mickel Jackson is a Pop Star"];
+// let choixDeJeu = prompt("Voulez-vous jouer avec des mots out des phrase, (0) pour Mots et (1) pour Phrase: ");
+// let score = 0;
 
 
-//functions
-const modeDeJeu = (choix) => {
-    return (choix === 0) ? listeMots : listePhrase;
-}
+// //functions
+// const modeDeJeu = (choix) => {
+//     return (choix === 0) ? listeMots : listePhrase;
+// }
 
-const jeu = (liste) => {
-    for (let i = 0; i < liste.length; i++) {
-        let motUtilisateur = prompt(`Entrer: ${liste[i]}`);
-        if (liste[i] === motUtilisateur) {
-            score++;
-        }
-        else {
-            i--;
-        }
-    }
-    return score;
-}
+// const jeu = (liste) => {
+//     for (let i = 0; i < liste.length; i++) {
+//         let motUtilisateur = prompt(`Entrer: ${liste[i]}`);
+//         if (liste[i] === motUtilisateur) {
+//             score++;
+//         }
+//         else {
+//             i--;
+//         }
+//     }
+//     return score;
+// }
 
-//test
-let partie = jeu(modeDeJeu(parseInt(choixDeJeu)));
+// //test
+// let partie = jeu(modeDeJeu(parseInt(choixDeJeu)));
 
-console.log(" votre score est: ", score);
-
+// console.log(" votre score est: ", score);
